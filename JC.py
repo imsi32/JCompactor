ab264d0d352be147bff67039bcc90509d544cfe3
imsi32 <81227251+imsi32@users.noreply.github.com>
2022-09-16 12:13:23 +0300

    Added JC.py and info.txt
    JC.py is the heart of this script. It has 2 modes: Single File and Directories (Not Finished)
    info.txt is a step by step plan which I write in Türkçe.

---
+++ JC.py
@@ -0,0 +1,42 @@
+"""Testing Json Module for JSON-Compactor"""
+import os
+import json
+
+def compactor(file):
+    """The main part of the script. It compacts JSON file."""
+    us={}
+    try:
+        with open(file, 'r', encoding="utf-8") as fr:
+            us.update(json.load(fr))
+        s1 = os.stat(file).st_size
+    except FileNotFoundError:
+        print(file, "Could not be found.")
+        return print("Try Again")
+    except json.decoder.JSONDecodeError:
+        return print(file, "is empty.")
+
+    with open(file, 'w', encoding="utf-8") as fw:
+        json.dump(us, fw)
+    s2 = os.stat(file).st_size
+    print(20 * "*", "\n"+ os.path.basename(file), "\nBefore:", str(s1), "\nAfter:", str(s2), "\nDifference:", str(s1 - s2), "\n"+ 20 * "*")
+    return s1 - s2
+
+
+print("Welcome to the JSON Composter.")
+while True:
+    print(80 * "*")
+    q1 = int(input("What will you compost? File[1] or Directory(2): "))
+
+    if q1 == 2:
+        pass
+    else:
+        # File Composting
+        f = input("What is file name: ")
+        compactor(f)
+
+    # Exit area
+    q2 = int(input("Do you want to exit? Yes[1] or No(2): "))
+    if q2 == 2:
+        pass
+    else:
+        break
